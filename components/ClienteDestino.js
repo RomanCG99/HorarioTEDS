481df6326f1b70821cdfd240506004a4e1a9cb22
drakorath <alam13900@gmail.com>
2022-09-13 17:07:48 -0500

    final

--- components/ClienteDestino.js
+++ components/ClienteDestino.js
@@ -1,12 +1,35 @@
 import React from 'react'
 
+function inputColor(Dia) {
+
+    if (Dia.cliente && Dia.destino) {
+        return {
+            label: "text-white",
+            input: "bg-green-600 text-white"
+        }
+    } 
+    if (Dia.cliente || Dia.destino) {
+        return {
+            label: "text-white",
+            input: "bg-amber-500 text-white"
+        }
+    } 
+    else {
+        return{
+
+            label : "bg-white text-blue-900",
+            input : "bg-white text-blue-900"
+        }
+    }
+}
 const ClienteDestino = (props) => {
-    const { label, inp, type, estilolabel, estiloinput } = props;
+    const { label, inp, type, estilolabel, estiloinput, color } = props;
+    const Color = inputColor(color)
 
     return (
-        <div className="flex md:w-full rounded py-1">
-            <label className={estilolabel}> {label} </label>
-            <input className={estiloinput} type={type} value={inp} />
+        <div className="flex md:w-full rounded py-2">
+            <label className= {Color.label + estilolabel} > {label} </label>
+            <input className= {Color.input + estiloinput} type={type} value={inp} />
         </div>
     )
 }
--- components/Lista.js
+++ components/Lista.js
@@ -13,53 +13,53 @@ const Lista = (props) => {
         }
     }
     return (
-        <div className='w-full grid grid-cols-8 gap-2 text-center  bg-gray-700'>
-            <div className='my-auto py-1'>
+        <div className='md:w-full grid grid-cols-8 gap-2 text-center bg-gray-700'>
+            <div className='flex my-auto py-1'>
                 <button>
                     <span class="material-symbols-outlined text-blue">edit</span>
                 </button>
                 <label className="text-md font-semibold text-blue-700"> {unidad.id} </label>
             </div>
-            <div className='py-1 text-center rounded'>
-                <div className={'w-full rounded' + getColors(unidad.LUNES)}>
-                    <ClienteDestino label="Cliente: " inp={unidad.LUNES.cliente} type="text" estilolabel="text-blue-900 font-semibold text-sm w-1/4 mx-2" estiloinput="w-3/4 font-semibold text-sm rounded focus:outline-none focus:border-b-2" />
-                    <ClienteDestino label="Destino: " inp={unidad.LUNES.destino} type="text" estilolabel="text-blue-900 font-semibold text-sm w-1/4 mx-2" estiloinput="w-3/4 font-semibold text-sm rounded focus:outline-none focus:border-b-2" />
+            <div className='w-full py-1'>
+                <div className={'py-1 text-center rounded ' + getColors(unidad.LUNES)}>
+                    <ClienteDestino color={unidad.LUNES} label="Cliente: " inp={unidad.LUNES.cliente} type="text" estilolabel= "  font-bold text-sm w-1/4 mx-2" estiloinput= " w-3/4 font-semibold text-sm rounded focus:outline-none mx-2 " />
+                    <ClienteDestino color={unidad.LUNES} label="Destino: " inp={unidad.LUNES.destino} type="text" estilolabel= "  font-bold text-sm w-1/4 mx-2" estiloinput= " w-3/4 font-semibold text-sm rounded focus:outline-none mx-2 " />
                 </div>
             </div>
             <div className='py-1 text-center rounded'>
-                <div className={'w-full rounded' + getColors(unidad.MARTES)}>
-                    <ClienteDestino label="Cliente: " inp={unidad.MARTES.cliente} type="text" estilolabel="text-blue-900 font-semibold text-sm w-1/4 mx-2" estiloinput="w-3/4 font-semibold text-sm rounded focus:outline-none focus:border-b-2" />
-                    <ClienteDestino label="Destino: " inp={unidad.MARTES.destino} type="text" estilolabel="text-blue-900 font-semibold text-sm w-1/4 mx-2" estiloinput="w-3/4 font-semibold text-sm rounded focus:outline-none focus:border-b-2" />
+                <div className={'py-1 text-center rounded  ' + getColors(unidad.MARTES)}>
+                    <ClienteDestino color={unidad.MARTES} label="Cliente: " inp={unidad.MARTES.cliente} type="text" estilolabel= " font-bold text-sm w-1/4 mx-2" estiloinput= " w-3/4 font-semibold text-sm rounded focus:outline-none mx-2 " />
+                    <ClienteDestino color={unidad.MARTES} label="Destino: " inp={unidad.MARTES.destino} type="text" estilolabel= " font-bold text-sm w-1/4 mx-2" estiloinput= " w-3/4 font-semibold text-sm rounded focus:outline-none mx-2 " />
                 </div>
             </div>
             <div className='py-1 text-center rounded'>
-                <div className={'w-full rounded' + getColors(unidad.MIERCOLES)}>
-                    <ClienteDestino label="Cliente: " inp={unidad.MIERCOLES.cliente} type="text" estilolabel="text-blue-900 font-semibold text-sm w-1/4 mx-2" estiloinput="w-3/4 font-semibold text-sm rounded focus:outline-none focus:border-b-2" />
-                    <ClienteDestino label="Destino: " inp={unidad.MIERCOLES.destino} type="text" estilolabel="text-blue-900 font-semibold text-sm w-1/4 mx-2" estiloinput="w-3/4 font-semibold text-sm rounded focus:outline-none focus:border-b-2" />
+                <div className={'py-1 text-center rounded  ' + getColors(unidad.MIERCOLES)}>
+                    <ClienteDestino color={unidad.MIERCOLES} label="Cliente: " inp={unidad.MIERCOLES.cliente} type="text" estilolabel= " font-bold text-sm w-1/4 mx-2" estiloinput= " w-3/4 font-semibold text-sm rounded focus:outline-none mx-2 " />
+                    <ClienteDestino color={unidad.MIERCOLES} label="Destino: " inp={unidad.MIERCOLES.destino} type="text" estilolabel= " font-bold text-sm w-1/4 mx-2" estiloinput= " w-3/4 font-semibold text-sm rounded focus:outline-none mx-2 " />
                 </div>
             </div>
             <div className='py-1 text-center rounded'>
-                <div className={'w-full rounded' + getColors(unidad.JUEVES)}>
-                    <ClienteDestino label="Cliente: " inp={unidad.JUEVES.cliente} type="text" estilolabel="text-blue-900 font-semibold text-sm w-1/4 mx-2" estiloinput="w-3/4 font-semibold text-sm rounded focus:outline-none focus:border-b-2" />
-                    <ClienteDestino label="Destino: " inp={unidad.JUEVES.destino} type="text" estilolabel="text-blue-900 font-semibold text-sm w-1/4 mx-2" estiloinput="w-3/4 font-semibold text-sm rounded focus:outline-none focus:border-b-2" />
+                <div className={'py-1 text-center rounded  ' + getColors(unidad.JUEVES)}>
+                    <ClienteDestino color={unidad.JUEVES} label="Cliente: " inp={unidad.JUEVES.cliente} type="text" estilolabel= " font-semibold text-sm w-1/4 mx-2" estiloinput= " w-3/4 font-semibold text-sm rounded focus:outline-none mx-2 " />
+                    <ClienteDestino color={unidad.JUEVES} label="Destino: " inp={unidad.JUEVES.destino} type="text" estilolabel= " font-semibold text-sm w-1/4 mx-2" estiloinput= " w-3/4 font-semibold text-sm rounded focus:outline-none mx-2 " />
                 </div>
             </div>
             <div className='py-1 text-center rounded'>
-                <div className={'w-full rounded' + getColors(unidad.VIERNES)}>
-                    <ClienteDestino label="Cliente: " inp={unidad.VIERNES.cliente} type="text" estilolabel="text-blue-900 font-semibold text-sm w-1/4 mx-2" estiloinput="w-3/4 font-semibold text-sm rounded focus:outline-none focus:border-b-2" />
-                    <ClienteDestino label="Destino: " inp={unidad.VIERNES.destino} type="text" estilolabel="text-blue-900 font-semibold text-sm w-1/4 mx-2" estiloinput="w-3/4 font-semibold text-sm rounded focus:outline-none focus:border-b-2" />
+                <div className={'py-1 text-center rounded  ' + getColors(unidad.VIERNES)}>
+                    <ClienteDestino color={unidad.VIERNES} label="Cliente: " inp={unidad.VIERNES.cliente} type="text" estilolabel= " font-semibold text-sm w-1/4 mx-2" estiloinput= " w-3/4 font-semibold text-sm rounded focus:outline-none mx-2 " />
+                    <ClienteDestino color={unidad.VIERNES} label="Destino: " inp={unidad.VIERNES.destino} type="text" estilolabel= " font-semibold text-sm w-1/4 mx-2" estiloinput= " w-3/4 font-semibold text-sm rounded focus:outline-none mx-2 " />
                 </div>
             </div>
             <div className='py-1 text-center rounded'>
-                <div className={'w-full rounded' + getColors(unidad.SABADO)}>
-                    <ClienteDestino label="Cliente: " inp={unidad.SABADO.cliente} type="text" estilolabel="text-blue-900 font-semibold text-sm w-1/4 mx-2" estiloinput="w-3/4 font-semibold text-sm rounded focus:outline-none focus:border-b-2" />
-                    <ClienteDestino label="Destino: " inp={unidad.SABADO.destino} type="text" estilolabel="text-blue-900 font-semibold text-sm w-1/4 mx-2" estiloinput="w-3/4 font-semibold text-sm rounded focus:outline-none focus:border-b-2" />
+                <div className={'py-1 text-center rounded  ' + getColors(unidad.SABADO)}>
+                    <ClienteDestino color={unidad.SABADO} label="Cliente: " inp={unidad.SABADO.cliente} type="text" estilolabel=" font-semibold text-sm w-1/4 mx-2" estiloinput= " w-3/4 font-semibold text-sm rounded focus:outline-none mx-2 " />
+                    <ClienteDestino color={unidad.SABADO} label="Destino: " inp={unidad.SABADO.destino} type="text" estilolabel=" font-semibold text-sm w-1/4 mx-2" estiloinput= " w-3/4 font-semibold text-sm rounded focus:outline-none mx-2 " />
                 </div>
             </div>
             <div className='py-1 text-center rounded' >
-                <div className={'w-full rounded' + getColors(unidad.DOMINGO)}>
-                    <ClienteDestino label="Cliente: " inp={unidad.DOMINGO.cliente} type="text" estilolabel="text-blue-900 font-semibold text-sm w-1/4 mx-2" estiloinput="w-3/4 font-semibold text-sm rounded focus:outline-none focus:border-b-2" />
-                    <ClienteDestino label="Destino: " inp={unidad.DOMINGO.destino} type="text" estilolabel="text-blue-900 font-semibold text-sm w-1/4 mx-2" estiloinput="w-3/4 font-semibold text-sm rounded focus:outline-none focus:border-b-2" />
+                <div className={'py-1 text-center rounded  ' + getColors(unidad.DOMINGO)}>
+                    <ClienteDestino color={unidad.DOMINGO} label="Cliente: " inp={unidad.DOMINGO.cliente} type="text" estilolabel=" font-semibold text-sm w-1/4 mx-2" estiloinput= " w-3/4 font-semibold text-sm rounded focus:outline-none mx-2 " />
+                    <ClienteDestino color={unidad.DOMINGO} label="Destino: " inp={unidad.DOMINGO.destino} type="text" estilolabel=" font-semibold text-sm w-1/4 mx-2" estiloinput= " w-3/4 font-semibold text-sm rounded focus:outline-none mx-2 " />
                 </div>
             </div>
         </div>
